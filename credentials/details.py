aeb6a028f668c905915207823e8517f54fb742ff
chetana <chetanajangale04@gmail.com>
2024-02-24 19:46:51 +0530

    cred

---
+++ credentials/details.py
@@ -0,0 +1,7 @@
+
+
+
+provider_url="https://eth-sepolia.g.alchemy.com/v2/FNRQa8jgrijpW15L_ZMpSr1wtiMI_00G"
+phrase = 'second combine wink raccoon polar tourist lend fame left sausage rotate carry'
+# Account address with which you will deploy the contract
+account_address = '0x08dA7a7816F11613993585CF0Dc9F2EA5b399a87'
